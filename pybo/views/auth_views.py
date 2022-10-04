b418bb1fb6f0bfba2edc1fd2553998294003838d
WooJin <110900977+NoooJin@users.noreply.github.com>
2022-10-04 21:52:09 +0900

    migrate

--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, url_for, render_template, flash, request, session
+from flask import Blueprint, url_for, render_template, flash, request, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
 from pybo import db
 from pybo.forms import UserCreateForm, UserLoginForm
 from pybo.models import User
+
+import functools
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -45,6 +47,32 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user.id
-            return redirect(url_for('main.index'))
+            _next = request.args.get('next', '')
+            if _next:
+                return redirect(_next)
+            else:
+                return redirect(url_for('main.index'))
         flash(error)
     return render_template('auth/login.html', form=form)
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(user_id)
+
+@bp.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('main.index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if g.user is None:
+            _next = request.url if request.method == "GET" else ''
+            return redirect(url_for('auth.login', next=_next))
+        return view(*args, **kwargs)
+    return wrapped_view
